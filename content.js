c597c727a03821184783d7ed70b8175b83d8aa75
Murendeni Mukwevho <murendeni@mukwevhom.xyz>
2024-11-13 00:26:10 +0200

    feat: add marca on websites list

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     let currentDomain = window.location.hostname
-
+    console.log("Ads PopUp", currentDomain)
     switch(currentDomain) {
         case 'www.businessinsider.com': {
             if (document.querySelector('body').classList.contains('tp-modal-open')) {
@@ -12,8 +12,11 @@ window.onload = function() {
         }
         case 'www.dailymail.co.uk':
         case 'www.foxnews.com':
-        case 'www.footyheadlines.com': {
+        case 'www.footyheadlines.com':
+        case 'www.marca.com': {
+            console.log("test")
             if(document.querySelector('.fc-ab-root')) {
+                console.log("test")
                 document.querySelector('.fc-ab-root').remove()
                 document.querySelector('body').style.overflowY = 'auto'
             }
